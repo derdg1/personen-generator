49cf1a2a5e628ddcb95014aeb483879f251aa5ea
derdg1 <dennis_graepel@me.com>
2025-06-23 17:03:48 +0200

    responsive/pwa

---
+++ static/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(event) {
+    console.log('Service Worker installiert');
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    // Standard-Fetch-Verhalten
+  });
+  
